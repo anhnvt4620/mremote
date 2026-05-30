7fda82ca348ccd321f0ea2f80ecd749fb7552cfb
M-Termius <anhnv@m-termius.dev>
2026-05-31 01:16:26 +0700

    fix: remove IPC from nsenter, add privileged mode

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 if [ -x /usr/bin/nsenter ]; then
   cat > /home/app/host-shell << 'WRAPPER'
 #!/bin/bash
-exec nsenter -t 1 -m -u -i -n -p -- su -l "${HOST_USER:-root}" -c "exec /bin/bash --login"
+exec nsenter -t 1 -m -u -n -p -- su -l "${HOST_USER:-root}" -c "exec /bin/bash --login"
 WRAPPER
   chmod +x /home/app/host-shell
   echo "[MRemote] host-shell ready (user: ${HOST_USER:-root})"
